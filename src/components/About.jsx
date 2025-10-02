6bc993fb22763dbe211a53bef728a3f573a5d8a6
nikhilcse0427 <nikhilcse0427@gmail.com>
2025-10-02 17:11:45 +0530

    Mounting lifecycle finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
-import Profile from './Profile'
-import ProfileClass from './ProfileClass'
+// import React from 'react'
+// import Profile from './Profile'
+// import ProfileClass from './ProfileClass'
 
-const userProfile = {
+// const userProfile = {
+//   name: "Nikhil Verma",
+//   age: 22,
+//   email: "abc@gmail.com",
+//   city: "Bhagalpur",
+//   state: "Bihar",
+//   nationality: "Indian"
+// }
+// const About = () => {
+//   return (
+//     <>
+//     <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+//       <Profile aboutUser={userProfile}/>
+//       <ProfileClass aboutUser={userProfile}/>
+//     </div>
+//     </>
+//   )
+// }
+
+// export default About
+
+import { Component } from 'react'
+import Profile from './Profile'
+import ProfileClass from './ProfileClass';
+import Profile from './Profile';
+
+class About extends Component{
+  constructor(){
+    super();
+    console.log("parent constructor is called")
+  }
+  
+  componentDidMount(){
+    console.log("parent mount is called")
+  }
+  userProfile = {
   name: "Nikhil Verma",
   age: 22,
   email: "abc@gmail.com",
@@ -10,15 +45,35 @@ const userProfile = {
   state: "Bihar",
   nationality: "Indian"
 }
-const About = () => {
-  return (
-    <>
-    <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-      <Profile aboutUser={userProfile}/>
-      <ProfileClass aboutUser={userProfile}/>
-    </div>
-    </>
-  )
+  render(){
+    console.log("parent render is called")
+    return(
+      <>
+      <div style={{display: "flex"}}>
+        {/* <Profile aboutUser={this.userProfile}/> */}
+        <ProfileClass aboutUser={this.userProfile}/>
+        <ProfileClass aboutUser={this.userProfile}/>
+      </div>
+      </>
+    )
+  }
 }
 
 export default About
+
+
+
+/**
+ * REACT LIFE CYCLE:
+ * parent constructor is called
+ * parent render is called
+ * child1 constructor is called
+ * child1 render is called
+ * child2 constructor is called
+ * child2 render is called
+ * child2 mount is called
+ * child1 mount is called
+ * parent mount is called
+ 
+ NOTE: mount is paused till component  get renderd till all component is called 
+ */
--- src/components/ProfileClass.jsx
+++ src/components/ProfileClass.jsx
@@ -4,11 +4,19 @@ class ProfileClass extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      count:0
+      count1:0,
+      count2:0
     }
+
+    console.log("child contstructor is called")
+  }
+
+  componentDidMount(){
+    console.log("child mount is called")
   }
   render(){
     const {name, age, email, city, state, nationality} = this.props.aboutUser
+    console.log("child reder is called")
     return (
       <div style={{height: "450px", width: "350px", border: "1px solid black", margin: "40px", padding: " 20px"}}>
       <h2 style={{textAlign: "center", color: "blue", textDecoration: "underline"}}>Class Based Component</h2>
@@ -18,12 +26,14 @@ class ProfileClass extends React.Component{
       <h3>City: {city}</h3>
       <h3>State: {state}</h3>
       <h3>Nationality: {nationality}</h3>
-      <h2>{this.state.count}</h2>
+      <h2>COUNT1: {this.state.count1}</h2>
+      <h2>COUNT2: {this.state.count2}</h2>
       <button onClick={()=>{
         // this.state.count = this.state.count+1
         // we cannot update state directly
         this.setState({
-          count: this.state.count+1
+          count1: this.state.count1+1,
+          count2: this.state.count2+1
         })
       }}>Increment</button>
     </div>
