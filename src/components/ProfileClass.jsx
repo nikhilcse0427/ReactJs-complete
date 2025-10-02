225fe126cf011a52cfd02fe4467951756f0253bf
nikhilcse0427 <nikhilcse0427@gmail.com>
2025-10-02 19:14:01 +0530

    mount, didUpdate and willMount Mount finished

--- src/components/ProfileClass.jsx
+++ src/components/ProfileClass.jsx
@@ -1,41 +1,58 @@
 import React from "react"
-
+import Skelleton from "./Skelleton";
+// github profile api https://api.github.com/users/nikhilcse0427
 class ProfileClass extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      count1:0,
-      count2:0
+      // count1:0,
+      // count2:0,
+      userProfile: null
     }
 
     console.log("child contstructor is called")
   }
 
-  componentDidMount(){
+  async componentDidMount(){
     console.log("child mount is called")
+    let data = await fetch("https://api.github.com/users/nikhilcse0427")//fetch and json promise return karata hai
+    let resData = await data.json()
+    console.log("JsonData: ",resData)
+    this.setState({
+      userProfile : resData
+    })
   }
+
+  componentDidUpdate(){
+    console.log("component did update")
+  }
+
+  /**This will come when we leave this page nest  */
+  componentWillUnmount(){
+    console.log("component will unmount")
+  }
+  
   render(){
-    const {name, age, email, city, state, nationality} = this.props.aboutUser
+    // const {name, age, email, city, state, nationality} = this.props.aboutUser
+    const {userProfile} = this.state
     console.log("child reder is called")
-    return (
-      <div style={{height: "450px", width: "350px", border: "1px solid black", margin: "40px", padding: " 20px"}}>
-      <h2 style={{textAlign: "center", color: "blue", textDecoration: "underline"}}>Class Based Component</h2>
-      <h3>Name: {name}</h3>
-      <h3>Age: {age}</h3>
-      <h3>Email: {email}</h3>
-      <h3>City: {city}</h3>
-      <h3>State: {state}</h3>
-      <h3>Nationality: {nationality}</h3>
-      <h2>COUNT1: {this.state.count1}</h2>
-      <h2>COUNT2: {this.state.count2}</h2>
-      <button onClick={()=>{
-        // this.state.count = this.state.count+1
-        // we cannot update state directly
-        this.setState({
-          count1: this.state.count1+1,
-          count2: this.state.count2+1
-        })
-      }}>Increment</button>
+
+    //conditional rendering
+    if(userProfile==null){
+      return <Skelleton />
+    }
+    const {login, name, email, location, bio, avatar_url} = userProfile
+
+    return(
+    <div style={{height: "550px", width: "280px", border: "1px solid black", margin: "40px", padding: " 20px", textAlign: "center"}}>
+      <img src={avatar_url} alt="avatar" style={{height:"150px"}} />
+      <h2 style={{textAlign: "center", color: "blue", textDecoration: "underline"}}>Github Profile</h2>
+      <h2>userName: <span style={{fontWeight:"normal", fontSize:"18px"}}>{login}</span></h2>
+      <h2>Name: <span style={{fontWeight:"normal", fontSize:"18px"}}>{name}</span></h2>
+      <h2>Email: <span style={{fontWeight:"normal", fontSize:"18px"}}>{email || "Not Available"}</span></h2>
+      {/* github often return null for email*/ }
+      <h2>Location: {location}</h2>
+      <h2 style={{textAlign: "center"}}>BIO: <p style={{fontWeight:"normal", fontSize:"18px"}}>{bio}</p></h2>
     </div>
       
     )
